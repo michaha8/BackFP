7a3c27e5f970f3e9a0cd400327a32d66ed435a4e
michael harush <michaelharush551@gmail.com>
2023-06-13 19:31:11 +0300

    Fixed Images

--- src/routes/file_route.ts
+++ src/routes/file_route.ts
@@ -10,7 +10,7 @@ const router = express.Router()
 
 import multer from 'multer'
 
-const base = "http://192.168.1.10:3000/"
+const base = "http://192.168.1.211:3000/"
 const storage = multer.diskStorage({
     destination: function (req: Request, file: unknown, cb) {
         cb(null, 'uploads/')
@@ -26,7 +26,8 @@ const upload = multer({ storage: storage });
 router.post('/file', upload.single("file"), function (req: Request, res: Response) {
     console.log(req.file.path)
     console.log("router.post(/file: " + base + req.file.path)
-    res.status(200).send({ url: base + req.file.path })
+    console.log("router.post(/file: "+'/' + req.file.path)
+    res.status(200).send({ url: '/' + req.file.path })
 });
 
 
